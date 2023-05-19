cbd91871285575ae24f3836136a486a1b7dbb767
adityasingh-fun <chaudharyaditya41@gmail.com>
2023-05-19 18:50:27 +0530

    get-api

--- src/index.js
+++ src/index.js
@@ -8,6 +8,12 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// mongoose.connect("mongodb+srv://chaudharyaditya41:Z67gI1uJnrGCnHuY@cluster0.jgngtnq.mongodb.net/Aditya-practice-db?retryWrites=true&w=majority", {
+//     useNewUrlParser: true
+// })
+// .then( () => console.log("MongoDb is connected"))
+// .catch ( err => console.log(err) )
+
 app.use('/', route);
 
 app.listen(process.env.PORT || 3000, function() {
--- src/routes/route.js
+++ src/routes/route.js
@@ -4,32 +4,78 @@ const router = express.Router();
 const commonFile = require('./common')
 const myUnderscore = require('underscore')
 
+
+
+let arr = ['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins'];
+
+router.get('/getMovies', function (req, res) {
+    res.send({ msg: arr })
+})
+
+
+router.get('/getMovies/:indexNumber', function (req, res) {
+    let index = req.params.indexNumber
+    if (index < 0 || index >= arr.length) {
+        return res.send({ msg: "Invalid Index" })
+    }
+    res.send({ msg: arr[index] })
+})
+
+let movieArr = [{
+    id: 1,
+    name: 'The Shining'
+}, {
+    id: 2,
+    name: 'Incendies'
+}, {
+    id: 3,
+    name: 'Rang de Basanti'
+}, {
+    id: 4,
+    name: 'Finding Nemo'
+}]
+
+router.get('/getMovieObject',function(req,res){
+    res.send({msg:movieArr})
+})
+
+router.get('/getMovieObject/:uniqueId',function(req,res){
+    let id = req.params.uniqueId
+    for(let i=0;i<movieArr.length;i++){
+        if(movieArr[i].id == id){
+            return res.send({msg:movieArr[i]})
+        }
+    }
+    res.send({msg:"Invalid id"})
+})
+
+
 router.get('/test-me', function (req, res) {
     res.send('This should be working!')
 });
 
 router.get('/test-you', function (req, res) {
-    console.log('The exported module is: ',commonFile)
+    console.log('The exported module is: ', commonFile)
     commonFile.doSomething()
     console.log('This is the constant I created', commonFile.name)
     res.send('Hello there, welcome to this application!')
 });
 
-router.get('/test-underscore', function(req, res){
-    let result = myUnderscore.first([11,12,23,44,15], 4)
-    console.log('the result is',result)
+router.get('/test-underscore', function (req, res) {
+    let result = myUnderscore.first([11, 12, 23, 44, 15], 4)
+    console.log('the result is', result)
     res.send('done')
 })
 
-router.get('/cohorts', function (request, response){
+router.get('/cohorts', function (request, response) {
     // logic to get the cohorts from database
     // logic tp get only the active cohorts
     // logic to get only the cohort with a size than 50
     // logic to get only the backend cohorts
-    response.send(['technetium','nobelium'])
+    response.send(['technetium', 'nobelium'])
 })
 
-router.get('/students', function(req, res){
+router.get('/students', function (req, res) {
     // receive or access the query params in the code
     // write a logic on these query params
     // city, score
@@ -37,10 +83,10 @@ router.get('/students', function(req, res){
     let requestedCity = req.query.city
     let sortField = req.query.sort
     // logic to get students
-    res.send(["Sabiha","Neha","Akash","Sonali"])
+    res.send(["Sabiha", "Neha", "Akash", "Sonali"])
 })
 
-router.get('/students/:studentName', function(req, res) {
+router.get('/students/:studentName', function (req, res) {
     console.log(req.params.studentName)
     /// go to database and search for studentName student
     // store the data found in this variable - studentDetails
